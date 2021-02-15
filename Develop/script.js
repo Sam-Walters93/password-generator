fa82773ad22b61cb8d3e23915b2a4e655f573571
Sam-Walters93 <stwalters314@gmail.com>
2021-02-15 11:02:37 -0600

    refactored input tests using switch statement

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,12 @@
 // Assignment code here
 
-
 generatePassword = () => {
   //set password length with user input 
-  var passLength = window.prompt("How long do you want your password? Choose a length between 8 and 128.");
+  var passLength = window.prompt("How long do you want your password? Choose a length between 8 and 128 characters.");
   
   //if entry is invalid make them try again 
   if (passLength < 8 || passLength > 128) {
     passLength = window.prompt("Sorry, that is not a valid option. \nPlease select a number between 8 and 128.");
-  }
-
-  //prompt to include lowercase letters. 
-  
-  var lowerCase = window.prompt("Would you like lowercase letters? Please enter 'yes' or 'no'");
-
-  if (lowerCase != 'yes' || lowerCase != 'no') {
-    lowerCase = window.prompt("Sorry, that is not a valid option. \nWould you like lowercase letters? Please enter 'yes' or 'no'");
-  }
-  
-  //prompt to include uppercase letters. 
-  var upperCase = window.prompt("Would you like uppercase letters? Please enter 'yes' or 'no'");
-
-  if (upperCase != 'yes' || upperCase != 'no') {
-    upperCase = window.prompt("Sorry, that is not a valid option. \nWould you like uppercase letters? Please enter 'yes' or 'no'");
-  }
-
-  //prompt to include numbers.
-  var numerical = window.prompt("Would you like numbers included? Please enter 'yes' or 'no'");
-
-  if (numerical != 'yes' || numerical != 'no') {
-    numerical = window.prompt("Sorry, that is not a valid option. \nWould you like numbers? Please enter 'yes' or 'no'");
-  }
-  
-  //prompt to include special characters. 
-  var specialChar = window.prompt("Would you like special characters? Please enter 'yes' or 'no'");
-
-  if (specialChar != 'yes' || specialChar != 'no') {
-    specialChar = window.prompt("Sorry, that is not a valid option. \nWould you like special characters? Please enter 'yes' or 'no'");
-  }
-
-  //check to see if any criteria was selected 
-  if ((lowerCase === 'no') && (upperCase === 'no') && (numerical === 'no') && (specialChar === 'no')) {
-    window.alert("ERROR: no criteria were selected, please try again.");
-    generatePassword();
   }
 
   //create holder array for user input characters
@@ -52,25 +16,31 @@ generatePassword = () => {
   var lowerCaseBank = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var upperCaseBank = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var numBank = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-  var specialCharBank = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', '=', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+  var specialCharBank = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', '=', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+
+  var passInput = window.prompt("What characters would you like included in your password? Enter the number associated with your choice. \n \n 1. LowerCase Letters \n 2. Lower and UpperCase letters \n 3. LowerCase letters, UpperCase letters, and Numbers. \n 4. LowerCase letters, UpperCase letters, Numbers, and Special Characters." )
+
+  switch (passInput) {
+    case '1':
+      passwordBank = passwordBank.concat(lowerCaseBank);
+      break;
+    case '2': 
+      passwordBank.push(...lowerCaseBank, ...upperCaseBank);
+      break;
+    case '3':
+      passwordBank.push(...lowerCaseBank, ...upperCaseBank, ...numBank);
+      break;
+    case '4': 
+      passwordBank.push(...lowerCaseBank, ...upperCaseBank, ...numBank, ...specialCharBank);
+      break;
+    default:
+      window.alert('ERROR: invalid input. Try Again');
+      generatePassword();
+  }
+
+  var password = '';
+
   
-  //set empty string to return 
-  let password = '';
-
-  //based on user input, concat character array(s) with holder array
-  if (lowerCase === 'yes') {
-    passwordBank = passwordBank.concat(lowerCaseBank);
-  }
-  if (upperCase === 'yes') {
-    passwordBank = passwordBank.concat(upperCaseBank);
-  }
-  if (numerical === 'yes') {
-    passwordBank = passwordBank.concat(numBank);
-  }
-  if (specialChar === 'yes') {
-    passwordBank = passwordBank.concat(specialCharBank);
-  }
-
   //for loop using user inputted password length 
   for (i = 0; i < passLength; i++) {
 
@@ -81,9 +51,6 @@ generatePassword = () => {
   //return final password 
   return password;
 };
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -98,3 +65,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
